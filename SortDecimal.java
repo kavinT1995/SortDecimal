3ae8d041d399914bcc3f900c044dd44462a9375d
kavinT1995 <kavin.tcse@ksrce.ac.in>
2016-07-22 17:36:42 +0530

    Create SortDecimal.java

---
+++ SortDecimal.java
@@ -0,0 +1,25 @@
+# SortDecimal
+public class SortDecimal {
+
+	public static void main(String[] args) {
+
+	Scanner sr=new Scanner(System.in);
+	System.out.println("Enter the size of array");
+	double size=sr.nextDouble();
+	   
+		   double iArr[] =new double[(int) size];
+		   System.out.println("enter the element");
+		   for(int i=0;i<iArr.length;i++){
+			   iArr[i]=sr.nextInt();
+		   }
+
+
+	   
+	   Arrays.sort(iArr);
+
+	   for(int j=iArr.length-1;j>=0;j--){
+		   System.out.print(iArr[j]+" ");
+	   }
+	   }
+
+	}
